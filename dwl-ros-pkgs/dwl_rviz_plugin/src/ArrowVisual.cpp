04d6c83c1dd519b7fdc5964a52e8b35a43165cfe
Carlos Mastalli <carlos.mastalli@iit.it>
2016-01-24 17:03:59 +0100

    Cosmetic changes

--- dwl-ros-pkgs/dwl_rviz_plugin/src/ArrowVisual.cpp
+++ dwl-ros-pkgs/dwl_rviz_plugin/src/ArrowVisual.cpp
@@ -14,15 +14,15 @@ ArrowVisual::ArrowVisual(Ogre::SceneManager* scene_manager,
 {
 	scene_manager_ = scene_manager;
 
-	// Ogre::SceneNode s form a tree, with each node storing the transform (position and
-	// orientation) of itself relative to its parent. Ogre does the math of combining those
-	// transforms when it is time to render.
-	// Here we create a node to store the pose of the Point's header frame relative to the RViz
-	// fixed frame.
+	// Ogre::SceneNode s form a tree, with each node storing the transform
+	// (position and orientation) of itself relative to its parent. Ogre does
+	// the math of combining those transforms when it is time to render.
+	// Here we create a node to store the pose of the Point's header frame
+	// relative to the RViz fixed frame.
 	frame_node_ = parent_node->createChildSceneNode();
 
-	// We create the arrow object within the frame node so that we can set its position and
-	// direction relative to its header frame.
+	// We create the arrow object within the frame node so that we can set its
+	// position and direction relative to its header frame.
 	arrow_ = new rviz::Arrow(scene_manager_, frame_node_);
 }
 
