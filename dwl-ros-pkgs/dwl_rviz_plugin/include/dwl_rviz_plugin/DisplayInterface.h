deeb49106e1d2f2545d634210faed06518e1172e
Carlos Mastalli <carlos.mastalli@iit.it>
2017-04-03 23:40:56 +0200

    [wip] Added more functionalities to the visualization interface

--- dwl-ros-pkgs/dwl_rviz_plugin/include/dwl_rviz_plugin/DisplayInterface.h
+++ dwl-ros-pkgs/dwl_rviz_plugin/include/dwl_rviz_plugin/DisplayInterface.h
@@ -1,6 +1,8 @@
 #ifndef DWL_RVIZ_PLUGIN__DISPLAY_INTERFACE__H
 #define DWL_RVIZ_PLUGIN__DISPLAY_INTERFACE__H
 
+#include <ros/NodeHandle.h>
+
 // Realtime tools include
 #include <realtime_tools/realtime_publisher.h>
 #include <realtime_tools/realtime_buffer.h>
@@ -11,28 +13,96 @@
 namespace dwl_rviz_plugin
 {
 
+enum ColorType {
+	RED,
+	GREEN,
+	BLUE,
+	YELLOW,
+	WHITE,
+	BLACK
+};
+
+struct Color
+{
+	Color(double _r, double _g, double _b, double _a) :
+		r(_r), g(_g), b(_b), a(_a) { }
+	Color(ColorType color, double _a) {
+		r = 0.;
+		g = 0.;
+		b = 0.;
+		a = _a;
+ 		if (color == ColorType::RED) {
+			r = 1.;
+		} else if (color == ColorType::GREEN) {
+			g = 1.;
+		} else if (color == ColorType::BLUE) {
+			b = 1.;
+		} else if (color == ColorType::YELLOW) {
+			r = 1.;
+			g = 1.;
+		} else if (color == ColorType::WHITE) {
+			r = 1.;
+			g = 1.;
+			b = 1.;
+		}
+	}
+
+	double r;
+	double g;
+	double b;
+	double a;
+}
+
+enum class displayType {
+	LINE = 0,
+	POINT = 1,
+	ARROW = 2,
+	SPHERE = 3
+};
+
+struct displayData {
+	Eigen::Vector3d p1;
+	Eigen::Vector3d p2;
+	Eigen::Vector3d scale;
+	Color color;
+	userGraphicsType type;
+};
+
 class DisplayInterface
 {
-	enum class userGraphicsType {
-		LINE = 0,
-		POINT = 1,
-		ARROW = 2,
-		SPHERE = 3
-	};
-
 	public:
 		DisplayInterface();
 		~DisplayInterface();
 
 		publishMarkerArray(const ros::Time& time);
 
-		drawLine();
+		void drawSphere(const Eigen::Vector3d& position,
+						double radiu,
+						const Color& color);
+		void drawArrow(const Eigen::Vector3d& begin,
+					   const Eigen::Vector3d& end,
+					   double shaft_diameter,
+					   double head_diameter,
+					   double head_length,
+					   const Color& color);
+		void drawCone(const Eigen::Vector3d& vertex,
+					  double height,
+					  double radiu,
+					  const Color& color);
+		void drawCone(const Eigen::Vector3d& vertex,
+					  double height,
+					  double angle,
+					  const Color& color);
 
 
 	private:
+		ros::NodeHandle node_;
 		boost::shared_ptr<realtime_tools::RealtimePublisher<visualization_msgs::MarkerArray> > markers_pub_;
 
+		/** @brief user stack for arbitrary graphics drawing */
+		std::vector<displayData> display_stack_;
 }
+
 } //@namespace dwl_rviz_plugin
 
 #endif
--- dwl-ros-pkgs/dwl_rviz_plugin/src/DisplayInterface.cpp
+++ dwl-ros-pkgs/dwl_rviz_plugin/src/DisplayInterface.cpp
@@ -7,7 +7,7 @@ namespace dwl_rviz_plugin
 
 DisplayInterface::DisplayInterface()
 {
-
+	markers_pub_.reset(new realtime_tools::RealtimePublisher<visualization_msgs::MarkerArray> (node_, "marker_array", 1));
 }
 
 
@@ -20,84 +20,140 @@ DisplayInterface::~DisplayInterface()
 void DisplayInterface::publishMarkerArray(const ros::Time& time)
 {
 	visualization_msgs::MarkerArray markers;
-	for (int i = 0; i < graphics_stack_.size(); i++) {
+	for (int i = 0; i < display_stack_.size(); i++) {
 		visualization_msgs::Marker marker;
 		marker.header.stamp = time;
 		marker.header.frame_id = "base_link";
 		marker.ns = "dls";
 		marker.id = i;
-		if (graphics_stack_[i].type == userGraphicsType::LINE) {
+		if (display_stack_[i].type == displayType::LINE) {
 			marker.type = visualization_msgs::Marker::LINE_LIST;
-			marker.color.r = graphics_stack_[i].r;
-			marker.color.g = graphics_stack_[i].g;
-			marker.color.b = graphics_stack_[i].b;
-			marker.color.a = 1.0;
-			marker.scale.x = 0.02;
+			marker.color.r = display_stack_[i].color.r;
+			marker.color.g = display_stack_[i].color.g;
+			marker.color.b = display_stack_[i].color.b;
+			marker.color.a = display_stack_[i].color.a;
+			marker.scale.x = display_stack_[i].scale(0);
 			geometry_msgs::Point p;
-			p.x = graphics_stack_[i].p1[0];
-			p.y = graphics_stack_[i].p1[1];
-			p.z = graphics_stack_[i].p1[2];
+			p.x = display_stack_[i].p1(0);
+			p.y = display_stack_[i].p1(1);
+			p.z = display_stack_[i].p1(2);
 			marker.points.push_back(p);
 
-			p.x = graphics_stack_[i].p2[0];
-			p.y = graphics_stack_[i].p2[1];
-			p.z = graphics_stack_[i].p2[2];
+			p.x = display_stack_[i].p2(0);
+			p.y = display_stack_[i].p2(1);
+			p.z = display_stack_[i].p2(2);
 			marker.points.push_back(p);
 			markers.markers.push_back(marker);
-		} else if (graphics_stack_[i].type == userGraphicsType::ARROW){
+		} else if (display_stack_[i].type == displayType::ARROW){
 			marker.type = visualization_msgs::Marker::ARROW;
-			marker.color.r = graphics_stack_[i].r;
-			marker.color.g = graphics_stack_[i].g;
-			marker.color.b = graphics_stack_[i].b;
-			marker.color.a = 1.0;
-			marker.scale.x = 0.02;
-			marker.scale.y = 0.05;
+			marker.color.r = display_stack_[i].color.r;
+			marker.color.g = display_stack_[i].color.g;
+			marker.color.b = display_stack_[i].color.b;
+			marker.color.a = display_stack_[i].color.a;
+			marker.scale.x = display_stack_[i].scale(0);
+			marker.scale.y = display_stack_[i].scale(1);
+			marker.scale.z = display_stack_[i].scale(2);
 			geometry_msgs::Point p;
-			p.x = graphics_stack_[i].p1[0];
-			p.y = graphics_stack_[i].p1[1];
-			p.z = graphics_stack_[i].p1[2];
+			p.x = display_stack_[i].p1(0);
+			p.y = display_stack_[i].p1(1);
+			p.z = display_stack_[i].p1(2);
 			marker.points.push_back(p);
 
-			p.x = graphics_stack_[i].p2[0];
-			p.y = graphics_stack_[i].p2[1];
-			p.z = graphics_stack_[i].p2[2];
+			p.x = display_stack_[i].p2(0);
+			p.y = display_stack_[i].p2(1);
+			p.z = display_stack_[i].p2(2);
 			marker.points.push_back(p);
 			markers.markers.push_back(marker);
-		} else if (graphics_stack_[i].type == userGraphicsType::POINT) {
+		} else if (display_stack_[i].type == displayType::POINT) {
 			marker.type = visualization_msgs::Marker::POINTS;
-			marker.color.r = graphics_stack_[i].r;
-			marker.color.g = graphics_stack_[i].g;
-			marker.color.b = graphics_stack_[i].b;
-			marker.color.a = 1.0;
-			marker.scale.x = 0.05;
-			marker.scale.y = 0.05;
-			marker.scale.z = 0.05;
+			marker.color.r = display_stack_[i].color.r;
+			marker.color.g = display_stack_[i].color.g;
+			marker.color.b = display_stack_[i].color.b;
+			marker.color.a = display_stack_[i].color.a;
+			marker.scale.x = display_stack_[i].scale(0);
+			marker.scale.y = display_stack_[i].scale(1);
+			marker.scale.z = display_stack_[i].scale(2);
 			marker.pose.orientation.w = 1.0;
-			marker.pose.position.x = graphics_stack_[i].p1[0];
-			marker.pose.position.y = graphics_stack_[i].p1[1];
-			marker.pose.position.z = graphics_stack_[i].p1[2];
-		} else if (graphics_stack_[i].type == userGraphicsType::SPHERE) {
+			marker.pose.position.x = display_stack_[i].p1(0);
+			marker.pose.position.y = display_stack_[i].p1(1);
+			marker.pose.position.z = display_stack_[i].p1(2);
+		} else if (display_stack_[i].type == displayType::SPHERE) {
 			marker.type = visualization_msgs::Marker::SPHERE;
-			marker.color.r = graphics_stack_[i].r;
-			marker.color.g = graphics_stack_[i].g;
-			marker.color.b = graphics_stack_[i].b;
-			marker.color.a = 1.0;
-			marker.scale.x = 0.05;
-			marker.scale.y = 0.05;
-			marker.scale.z = 0.05;
+			marker.color.r = display_stack_[i].color.r;
+			marker.color.g = display_stack_[i].color.g;
+			marker.color.b = display_stack_[i].color.b;
+			marker.color.a = display_stack_[i].color.a;
+			marker.scale.x = display_stack_[i].scale(0);
+			marker.scale.y = display_stack_[i].scale(1);
+			marker.scale.z = display_stack_[i].scale(2);
 			marker.pose.orientation.w = 1.0;
-			marker.pose.position.x = graphics_stack_[i].p1[0];
-			marker.pose.position.y = graphics_stack_[i].p1[1];
-			marker.pose.position.z = graphics_stack_[i].p1[2];
+			marker.pose.position.x = display_stack_[i].p1(0);
+			marker.pose.position.y = display_stack_[i].p1(1);
+			marker.pose.position.z = display_stack_[i].p1(2);
 			markers.markers.push_back(marker);
 		}
-
 	}
 	
+	display_stack_.clear();
 	if (markers_pub_->trylock()) {
 		markers_pub_->msg_ = markers;
 		markers_pub_->unlockAndPublish();
 	}
+}
+
+
+void DisplayInterface::drawSphere(const Eigen::Vector3d& position,
+								  double radiu,
+								  const Color& color)
+{
+	displayData data;
+	data.p1 = position;
+	data.scale = Eigen::Vector3d(radiu, radiu, radiu);
+	data.color = color;
+	data.type = displayType::SPHERE;
+	display_stack_.push_back(data);
+}
+
+
+void DisplayInterface::drawArrow(const Eigen::Vector3d& begin,
+								 const Eigen::Vector3d& end,
+								 double shaft_diameter,
+								 double head_diameter,
+								 double head_length,
+								 const Color& color)
+{
+	displayData data;
+	data.p1 = begin;
+	data.p2 = end;
+	data.scale = Eigen::Vector3d(shaft_diameter, head_diameter, head_length);
+	data.color = color;
+	data.type = displayType::ARROW;
+	display_stack_.push_back(data);
+}
+
+
+void DisplayInterface::drawCone(const Eigen::Vector3d& vertex,
+								double height,
+								double radiu,
+								const Color& color)
+{
+	displayData data;
+	data.p1 = vertex;
+	data.p2 = vertex + Eigen::Vector3d(0., 0., height); // TODO orientation
+	data.scale = Eigen::Vector3d(0., 2 * radiu, height);
+	data.color = color;
+	data.type = displayType::ARROW;
+	display_stack_.push_back(data);
+}
+
+
+void DisplayInterface::drawCone(const Eigen::Vector3d& vertex,
+								double height,
+								double angle_rad,
+								const Color& color)
+{
+	double radiu = height * atan(angle_rad);
+	drawCone(vertex, height, radiu, color);
 }
 
 } //@namespace dwl_rviz_plugin
